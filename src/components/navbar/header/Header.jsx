9da7b69ed48188dd0c49c4ea0170bb7100cd7240
Ishhit <satybaldiev.ishhit@gmail.com>
2023-11-25 17:47:36 +0500

    Add header css  and font

--- src/components/navbar/header/Header.jsx
+++ src/components/navbar/header/Header.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
+import "./Header.css"
+
 class Header extends Component {
     render() {
         return (
             <>
                 <nav className="header-menu">
+                    <Link to="/" className="header-menu-logo">
+                        React To-Do
+                    </Link>
                     <ul className="header-menu-list">
                         <li className="header-menu-li">
                             <Link to="/" className="header-menu-link">
